50e1fcfdf889af87b0ef489f7536f0e1e07f936a
Ashley Maciejewski <ash.maciejewski@gmail.com>
2019-10-05 18:15:32 -0500

    Configured middleware to parse json

--- server.js
+++ server.js
@@ -8,9 +8,24 @@ const app = express();
 connectDatabase();
 
 //api endpoints
+/**
+ *@route GET/
+ *@desc test endpoints/
+ **/
+
 app.get('/', (req, res) =>
     res.send('http get request sent to api endpoint')
 );
+
+/**
+ *@route POST api/users
+ *@desc register user
+ **/
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 //connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
