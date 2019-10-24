953ba316cce7505f6516492da709ea7198a6412e
Ashley Maciejewski <ash.maciejewski@gmail.com>
2019-10-23 22:00:38 -0500

    Updated Activity 5

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import User from './models/Users';
+//import { isError } from 'util';
 
-//initialize express application
+
+//init express app
 const app = express();
 
-//connect database
 connectDatabase();
 
 //configure middleware
-app.use(express.json({ extended : false}));
+app.use(express.json({ extended: false }));
 app.use(
     cors({
         origin: 'http://localhost:3000'
@@ -19,39 +24,74 @@ app.use(
 
 //api endpoints
 /**
- *@route GET/
- *@desc test endpoint/
+ * @route GET /
+ * @desc Test endpoint
  */
 app.get('/', (req, res) =>
-    res.send('http get request sent to api endpoint')
+    res.send('http get request sent to root api endpoint')
 );
 
 /**
- *@route POST api/users  
- *@desc register user
+ * @route Post api/users
+ * @desc Register user
  */
 app.post(
-    '/api/Users',
+    '/api/users',
     [
-        check('name', 'Please enter your name')
-        .not()
-        .isEmpty(),
-        check('email', 'PLease enter a valid email').isEmail(),
-        check(
-            'password',
-            'Please enter a password with 6 or more characters'
-        ).isLength({ min: 6})
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         } else {
-            return res.send(req.body);
+            const { name, email, password } = req.body;
+            try{
+                //check if user exists
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res.status(400).json({errors: [{msg: 'User already exists'}] });
+                }
+
+                //new user
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                //password encryption
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                //save and return
+                await user.save();
+
+                //generate jwt token
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: '10hr' },
+                    (err, token) => {
+                        if (err) throw err;
+                        res.json({ token: token });
+                    }
+                );
+                //res.send('User successfully registered');
+            } catch (error){
+                res.status(500).send('Server error');
+            }  
         }
     }
 );
-
 //connection listener
 const port = 5000;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
